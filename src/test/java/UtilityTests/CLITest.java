14c0dfbf50cfffa5f34468bbbcd9fc013c62b968
rrazumilovs <razumilovrenat@inbox.lv>
2022-02-17 12:04:11 +0200

    cli options parser - clean

--- src/test/java/UtilityTests/CLITest.java
+++ src/test/java/UtilityTests/CLITest.java
@@ -1,15 +1,8 @@
 package UtilityTests;
 
 import com.xfus10n.twitter.Utilz.CLI;
-import com.xfus10n.twitter.domainz.TwitterProperties;
-import com.xfus10n.twitter.propertiesReader.Reader;
 import org.apache.commons.cli.CommandLine;
 import org.junit.Test;
-import scala.util.parsing.combinator.testing.Str;
-
-import java.util.Properties;
-
-import static com.xfus10n.twitter.domainz.TwitterProperties.*;
 import static org.junit.Assert.*;
 
 public class CLITest {
